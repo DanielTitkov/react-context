0a0e7065412bde9900a7c377a8b1f72a51a2b941
DanielTitkov <mytasmic@gmail.com>
2019-08-21 18:21:29 +0300

    booklist

--- booklist/src/components/Navbar.js
+++ booklist/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react'
+import { BookContext } from '../contexts/BookContext';
 
 export default function Navbar() {
+    const { books } = useContext(BookContext);
     return (
-        <div>
-            
+        <div className="navbar">
+            <h1>Reading list</h1>
+            <p>Currently have { books.length } books</p>
         </div>
     )
 }
